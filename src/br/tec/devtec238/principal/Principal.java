64087bc705f56e9c887f3e6b1d325aa71ef6e319
Rubenildo <rubenildo.ferreira048@gmail.com>
2024-05-08 11:34:14 -0300

    First commit

---
+++ src/br/tec/devtec238/principal/Principal.java
@@ -0,0 +1,13 @@
+package br.tec.devtec238.principal;
+
+import br.tec.devtec238.funcao.Funcoes;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		//BLOCO - Declaração de variável
+		Funcoes func = new Funcoes();
+
+		func.mediaAritmetica();
+			}//fechamento method main
+		}//fechamento da classe
